841a566a4ac53b16a2fbc78bcb11a8bdbdc3971e
Hoseyn <hoseyn.mousavi78@gmail.com>
2021-05-17 00:31:22 +0430

    fx

--- src/views/containers/HomeGuide.js
+++ src/views/containers/HomeGuide.js
@@ -1,4 +1,4 @@
-import {useState} from "react"
+import {useRef} from "react"
 import PhoneSvg from "../../media/svgs/PhoneSvg"
 import Picture from "../components/Picture"
 import mockups1 from "../../media/images/mockups-1.png"
@@ -7,30 +7,44 @@ import mockups2 from "../../media/images/mockups-2.png"
 import mockups2webp from "../../media/webp/mockups-2.webp"
 import mockups3 from "../../media/images/mockups-3.png"
 import mockups3webp from "../../media/webp/mockups-3.webp"
+import ScrollY from "../../helpers/ScrollY"
+import GuideBgSvg from "../../media/svgs/GuideBgSvg"
+import TriangleSvg from "../../media/svgs/TriangleSvg"
 
 function HomeGuide()
 {
-    const [guideLevel] = useState(0)
+    const contRef = useRef(null)
+    let offsetTop
+    if (contRef?.current && !offsetTop) offsetTop = contRef.current.offsetTop
+    const headerHeight = +(process.env.REACT_APP_HEADER_HEIGHT.replace("px", ""))
+    const scrollY = ScrollY()
+    const margin = Math.min(Math.max(0, scrollY + headerHeight - offsetTop), 800)
+    const guideLevel = margin > 600 ? 3 : margin > 400 ? 2 : margin > 200 ? 1 : 0
+
     return (
-        <div className="home-guide">
-            {/*<GuideBgSvg className="home-guide-bg"/>*/}
-            {/*<GuideBgSvg className="home-guide-bg-down"/>*/}
-            {/*<TriangleSvg className="home-download-triangle"/>*/}
-            <div className="home-guide-inner">
+        <div className="home-guide" ref={contRef}>
+            <GuideBgSvg className="home-guide-bg"/>
+            <GuideBgSvg className="home-guide-bg-down"/>
+            <TriangleSvg className="home-download-triangle"/>
+            <div className="home-guide-inner" style={{transform: `translate3d(0,${margin}px,0)`}}>
                 <div className="home-guide-title">چطور از بومینو استفاده کنم؟</div>
                 <div className="home-guide-content">
                     <div className="home-guide-content-text">
-                        <div className={`home-guide-item ${guideLevel === 0 ? "" : "disable"}`}>
-                            <div className={`home-guide-item-title ${guideLevel === 0 ? "" : "disable"}`}>1. دانلود</div>
+                        <div className={`home-guide-item ${guideLevel === 3 ? "hide" : guideLevel === 0 ? "" : "disable"}`}>
+                            <div className={`home-guide-item-title ${guideLevel === 0 ? "" : "disable"}`}>1. دانلود و نصب</div>
                             <div>ابتدا اپلیکیشن را دانلود<br/>و نصب کنید.</div>
                         </div>
                         <div className={`home-guide-item ${guideLevel === 1 ? "" : "disable"}`}>
-                            <div className={`home-guide-item-title ${guideLevel === 1 ? "" : "disable"}`}>2. ساخت اکانت</div>
-                            <div>ثبت‌نام کنید و فرزند یا<br/>فرزندانتان را تعریف کنید.</div>
+                            <div className={`home-guide-item-title ${guideLevel === 1 ? "" : "disable"}`}>2. ساخت پروفایل</div>
+                            <div>بعد ثبت‌نام، برای فرزند یا فرزندانتون<br/>پروفایل بسازید.</div>
                         </div>
                         <div className={`home-guide-item ${guideLevel === 2 ? "" : "disable"}`}>
-                            <div className={`home-guide-item-title ${guideLevel === 2 ? "" : "disable"}`}>3. فعال‌کردن اینترنت کودک</div>
-                            <div>اینترنت را فعال کنید و<br/>با خیال آسوده<br/>به فرزندتان بسپارید.</div>
+                            <div className={`home-guide-item-title ${guideLevel === 2 ? "" : "disable"}`}>3. انتخاب دسترسی به اینترنت</div>
+                            <div>سپس می‌تونید یکی از بسته‌های مناسب<br/>ردهٔ سنی فرزندتون رو انتخاب کنید.</div>
+                        </div>
+                        <div className={`home-guide-item ${guideLevel === 3 ? "" : "hide"}`}>
+                            <div className={`home-guide-item-title ${guideLevel === 3 ? "" : "disable"}`}>4. بچه‌تونو بفرستید اینترنت</div>
+                            <div>با خیال راحت گوشی رو به فرزندتون بدید. اینجا کلی<br/>ابزار و محتوای جذاب رو هم بهش معرفی می‌کنیم.</div>
                         </div>
                     </div>
                     <div className="home-guide-dot-cont">
