58df16611da615edf24bf9cd39bd92f88d0756cf
Hoseyn <hoseyn.mousavi78@gmail.com>
2021-05-23 01:36:03 +0430

    fx

--- src/views/containers/Home/HomeDownload.js
+++ src/views/containers/Home/HomeDownload.js
@@ -9,16 +9,39 @@ import {useRef} from "react"
 
 function HomeDownload()
 {
+    let guideTop = useRef(null)
+    let guideHeight = useRef(null)
+    let phoneTop = useRef(null)
+    let phoneHeight = useRef(null)
+    const guideRef = document.getElementById("home-guide")
     const phoneRef = useRef(null)
+    if (guideRef && phoneRef.current && !guideTop.current && !phoneTop.current)
+    {
+        guideTop.current = guideRef.offsetTop
+        guideHeight.current = guideRef.scrollHeight
+        phoneTop.current = phoneRef.current.offsetTop
+        phoneHeight.current = phoneRef.current.scrollHeight
+    }
     const scrollY = ScrollY()
-    console.log(scrollY, phoneRef?.current?.offsetTop)
+    const headerHeight = +(process.env.REACT_APP_HEADER_HEIGHT.replace("px", "")) - +(process.env.REACT_APP_HEADER_LOW_HEIGHT.replace("px", ""))
+    const margin = Math.min(Math.max(0, scrollY + window.innerHeight / 2 - headerHeight - phoneTop.current - 50), guideTop.current - phoneTop.current)
+    const transform = `translate3d(
+    calc((-100vw + 30vw + 200px + 50px) * ${margin / (guideTop.current - phoneTop.current)}),
+    ${
+        scrollY + window.innerHeight / 2 - headerHeight > guideTop.current + guideHeight.current - phoneHeight.current / 2 ?
+            guideTop.current + guideHeight.current - phoneHeight.current / 2 - (scrollY + window.innerHeight / 2 - headerHeight)
+            :
+            0
+    }px,
+    0
+    )`
     return (
         <div className="home-download-app" ref={phoneRef}>
-            <AppBgSvg className="home-download-app-bg"/>
-            <div className="home-download-app-svg-cont">
+            <AppBgSvg className={`home-download-app-bg ${margin > 0 ? "hide" : ""}`}/>
+            <div className="home-download-app-svg-cont" style={{transform}}>
                 <PhoneSvg className="home-download-app-svg"/>
             </div>
-            <div className="home-download-app-content">
+            <div className={`home-download-app-content ${margin > 0 ? "hide" : ""}`}>
                 <div className="home-download-app-title">همین حالا اپلیکیشن بومینو رو نصب کنید</div>
                 <div className="home-download-app-desc">استفاده از بومینو کاملا رایگانه</div>
                 <div className="home-download-app-store">
