c4a5f3826bdd60f98e9b094c8224d97db6a135d8
Hoseyn <hoseyn.mousavi78@gmail.com>
2021-06-08 14:36:53 +0430

    fx

--- src/views/components/SocialIcons.js
+++ src/views/components/SocialIcons.js
@@ -1,18 +1,16 @@
-import TwitterSvg from "../../media/svgs/TwitterSvg"
 import InstagramSvg from "../../media/svgs/InstagramSvg"
 import LinkedinSvg from "../../media/svgs/LinkedinSvg"
-import WhatsappSvg from "../../media/svgs/WhatsappSvg"
 import TelegramSvg from "../../media/svgs/TelegramSvg"
 
 function SocialIcons()
 {
     return (
         <>
-            <TwitterSvg className="footer-top-right-social-svg"/>
-            <InstagramSvg className="footer-top-right-social-svg"/>
-            <LinkedinSvg className="footer-top-right-social-svg"/>
-            <WhatsappSvg className="footer-top-right-social-svg"/>
-            <TelegramSvg className="footer-top-right-social-svg"/>
+            {/*<a className="footer-top-right-social-svg-link" href="https://www.instagram.com/boomino_official" target="_blank" rel="noreferrer"><TwitterSvg className="footer-top-right-social-svg"/></a>*/}
+            <a className="footer-top-right-social-svg-link" href="https://www.instagram.com/boomino_official" target="_blank" rel="noreferrer"><InstagramSvg className="footer-top-right-social-svg"/></a>
+            <a className="footer-top-right-social-svg-link" href="https://www.linkedin.com/company/mci-boomino" target="_blank" rel="noreferrer"><LinkedinSvg className="footer-top-right-social-svg"/></a>
+            {/*<WhatsappSvg className="footer-top-right-social-svg"/>*/}
+            <a className="footer-top-right-social-svg-link" href="https://t.me/boomino_official" target="_blank" rel="noreferrer"><TelegramSvg className="footer-top-right-social-svg"/></a>
         </>
     )
 }
